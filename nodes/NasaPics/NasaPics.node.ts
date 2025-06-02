fc286c9e149a5235590ca306d8142ec102d1eb23
Atharva Domale <115187316+AtharvaDomale@users.noreply.github.com>
2025-06-02 16:46:22 +0000

    made changes in Nasapics ts file

---
+++ nodes/NasaPics/NasaPics.node.ts
@@ -0,0 +1,37 @@
+import { INodeType, INodeTypeDescription } from 'n8n-workflow';
+
+export class NasaPics implements INodeType {
+	description: INodeTypeDescription = {
+		// Basic node details will go here
+		properties: [
+		// Resources and operations will go here
+		]
+	};
+}
+
+displayName: 'NASA Pics',
+name: 'NasaPics',
+icon: 'file:nasapics.svg',
+group: ['transform'],
+version: 1,
+subtitle: '={{$parameter["operation"] + ": " + $parameter["resource"]}}',
+description: 'Get data from NASAs API',
+defaults: {
+	name: 'NASA Pics',
+},
+inputs: ['main'],
+outputs: ['main'],
+credentials: [
+	{
+		name: 'NasaPicsApi',
+		required: true,
+	},
+],
+requestDefaults: {
+	baseURL: 'https://api.nasa.gov',
+	headers: {
+		Accept: 'application/json',
+		'Content-Type': 'application/json',
+	},
+},
+
